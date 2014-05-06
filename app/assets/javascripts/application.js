db2ee703bcd0814171cade59059bb3197a0b0f5b
Amy Mok <amymok@codeforamerica.org>
2014-05-06 12:54:14 -0700

    Add Prototype frontend in the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require webflow
+//= require modernizr
+//= require jquery-waypoints/waypoints.min
+//= require landing
---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,46 @@
+  $(document).ready(function() 
+  {
+    $(".roof-btn").click(function()
+    {
+      $(".roof").fadeToggle("fast","swing");
+      $('.roof-btn').toggleClass("toggle-on");
+    });
+
+    $(".fence-btn").click(function()
+    {
+      $(".fence").fadeToggle("fast","swing");
+      $('.fence-btn').toggleClass("toggle-on");
+    });
+
+    $(".repairs-btn").click(function()
+    {
+      $(".chimney").fadeToggle("fast","swing");
+      $('.repairs-btn').toggleClass("toggle-on");
+    });
+
+    $(".addition-btn").click(function()
+    {
+      $(".addition").fadeToggle("fast","swing");
+      $('.addition-btn').toggleClass("toggle-on");
+    });
+
+    $('.ha-waypoint').waypoint(function(direction) 
+    {
+      var $el = $(this);
+      var percentage = $el.data(direction === 'down' ? 'animateDown' : 'animateUp');
+
+      $('.waypoint').animate(
+      {
+        width: percentage,
+      }, 
+      {
+        duration: 300,
+      });
+
+      $('.progress-percent').html(percentage)
+        // $('.waypoint').removeClass("step-0 step-1").addClass(animClass);
+      }, 
+      { 
+        offset: '70%' 
+      });
+ });
