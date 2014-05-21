64c9457d69c48b8a912abeba12b2ef3b9aac1a6d
Amy Mok <amymok@codeforamerica.org>
2014-05-21 11:59:23 -0700

    Check whether in SA only if address is not blank

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -1,5 +1,6 @@
 class Permit < ActiveRecord::Base
-  validates :owner_address,      :presence => true, :address => true, :if => :active_or_address?
+  validates :owner_address, :presence => true, :if => :active_or_address?
+  validates :owner_address, :address => true, :if => :only_if_presence?
 
 
   def active?
@@ -8,5 +9,9 @@ class Permit < ActiveRecord::Base
 
   def active_or_address?
     status.to_s.include?('enter_address') || active?
+  end
+
+  def only_if_presence?
+    active_or_address? && ! owner_address.blank?
   end
 end
