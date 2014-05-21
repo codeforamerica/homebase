b7e47058ec26ad4b5c199b49a45145881b724ad3
Amy Mok <amymok@codeforamerica.org>
2014-05-20 19:48:13 -0700

    Try changing initializer to see if it works in heroku

--- config/initializers/postgis.rb
+++ config/initializers/postgis.rb
@@ -1,9 +1,30 @@
 Rails.application.config.after_initialize do
   ActiveRecord::Base.connection_pool.disconnect!
 
+  # ActiveSupport.on_load(:active_record) do
+  #   config = Rails.application.config.database_configuration[Rails.env]
+  #   config['adapter'] = 'postgis'
+  #   ActiveRecord::Base.establish_connection(config)
+  # end
+
+  #   initializer "active_record.initialize_database.override" do |app|
+
   ActiveSupport.on_load(:active_record) do
-    config = Rails.application.config.database_configuration[Rails.env]
-    config['adapter'] = 'postgis'
-    ActiveRecord::Base.establish_connection(config)
+    if url = ENV['DATABASE_URL']
+      ActiveRecord::Base.connection_pool.disconnect!
+      parsed_url = URI.parse(url)
+      config =  {
+        adapter:             'postgis',
+        host:                parsed_url.host,
+        encoding:            'unicode',
+        database:            parsed_url.path.split("/")[-1],
+        port:                parsed_url.port,
+        username:            parsed_url.user,
+        password:            parsed_url.password
+      }
+      establish_connection(config)
+    end
   end
 end
+#end
+
