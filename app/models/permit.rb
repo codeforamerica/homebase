fe9e8f47e427f52f15863fa8cfd2a5e634e23e03
Amy Mok <amymok@codeforamerica.org>
2014-05-27 13:48:28 -0700

    Add validations for all required field

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -1,8 +1,14 @@
 class Permit < ActiveRecord::Base
   validates_presence_of :owner_address, :if => :active_or_address?, :message => "Please enter a San Antonio address."
   validates :owner_address, :address => true, :if => :only_if_address_presence?
+  validates_presence_of :owner_name, :if => :active_or_details?, :message => "Please enter home owner name."
+  validates_presence_of :house_area, :if => :active_or_details?, :message => "Please enter the size of house in square feet."
+  validates_numericality_of :house_area, :if => :only_if_house_presence?, :message => "Please enter the size of house in square feet."
   validates_presence_of :addition_area, :if => :active_or_details?, :message => "Please enter the size of addition in square feet."
   validates_numericality_of :addition_area, less_than: 1000, :if => :only_if_addition_presence?, :message => "Addition must be less than 1,000 Square Feet."
+  validates_presence_of :ac, :if => :active_or_details?, :message => "Please select an air conditioning / heating system."
+  validates_presence_of :work_summary, :if => :active_or_details?, :message => "Please enter a work summary."
+  validates_presence_of :job_cost, :if => :active_or_details?, :message => "Please enter the job cost."
 
 
   def active?
@@ -23,5 +29,9 @@ class Permit < ActiveRecord::Base
 
   def only_if_addition_presence?
     active_or_details? && ! addition_area.blank?
+  end
+
+  def only_if_house_presence?
+    active_or_details? && ! house_area.blank?
   end
 end
