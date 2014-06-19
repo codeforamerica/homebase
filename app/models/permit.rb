f261757bf7127ea44eb0007864c4d22b0c06f2fa
Amy Mok <amymok@codeforamerica.org>
2014-06-19 14:45:29 -0700

    Add validation on addition area, job cost, re-selection of ac when an error occurred.  Attempt to reselect contractor, but didn't work out yet, will fix in another branch

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -12,11 +12,13 @@ class Permit < ActiveRecord::Base
   validates_presence_of :house_area, :if => :active_or_details?, :message => "Please enter the size of house in square feet."
   validates_numericality_of :house_area, :if => :only_if_house_presence?, :message => "Please enter the size of house in square feet."
   validates_presence_of :addition_area, :if => :active_or_details?, :message => "Please enter the size of addition in square feet."
+  validates_numericality_of :addition_area, :if => :only_if_addition_presence?, :message => "Please enter the size of addition in square feet."
   validates_numericality_of :addition_area, less_than: 1000, :if => :only_if_addition_presence?, :message => "Addition must be less than 1,000 Square Feet."
   validates_presence_of :ac, :if => :active_or_details?, :message => "Please select an air conditioning / heating system."
   validates_inclusion_of :contractor, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are using a contractor or not in this project."
   validates_presence_of :work_summary, :if => :active_or_details?, :message => "Please enter a work summary."
   validates_presence_of :job_cost, :if => :active_or_details?, :message => "Please enter the job cost."
+  validates_numericality_of :job_cost, :if => :only_if_job_cost_presence?, :message => "Job cost must be a number."
 
 
   def active?
@@ -41,5 +43,9 @@ class Permit < ActiveRecord::Base
 
   def only_if_house_presence?
     active_or_details? && ! house_area.blank?
+  end
+
+  def only_if_job_cost_presence?
+    active_or_details? && ! job_cost.blank?
   end
 end
