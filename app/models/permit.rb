650542b52c511fdbc8c4f79df0df99af53e245fd
Amy Mok <amymok@codeforamerica.org>
2014-07-08 09:46:17 -0700

    Fix validation to validate on enter_details page, and add validation on first page to make sure at least one improvement is chosen

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -2,6 +2,7 @@ class Permit < ActiveRecord::Base
   
   # validates on permit_steps#new
   # validates_inclusion_of :addition, :in => [true], :message => "Please choose an improvement."
+  validate :at_least_one_chosen
 
   # validates on permit_steps#enter_address
   validates_presence_of :owner_address, :if => :active_or_address?, :message => "Please enter a San Antonio address."
@@ -15,21 +16,21 @@ class Permit < ActiveRecord::Base
   validates_numericality_of :job_cost, :if => :only_if_job_cost_presence?, :message => "Job cost must be a number."
 
   # validates on permit_steps#enter_addition
-  validates_presence_of :house_area, :if => :active_or_addition?, :message => "Please enter the size of house in square feet."
+  validates_presence_of :house_area, :if => :active_or_details?, :message => "Please enter the size of house in square feet."
   validates_numericality_of :house_area, :if => :only_if_house_presence?, :message => "Please enter the size of house in square feet."
-  validates_presence_of :addition_area, :if => :active_or_addition?, :message => "Please enter the size of addition in square feet."
+  validates_presence_of :addition_area, :if => :active_or_details?, :message => "Please enter the size of addition in square feet."
   validates_numericality_of :addition_area, :if => :only_if_addition_presence?, :message => "Please enter the size of addition in square feet."
   validates_numericality_of :addition_area, less_than: 1000, :if => :only_if_addition_presence?, :message => "Addition must be less than 1,000 Square Feet."
-  validates_presence_of :ac, :if => :active_or_addition?, :message => "Please select an air conditioning / heating system."
+  validates_presence_of :ac, :if => :active_or_details?, :message => "Please select an air conditioning / heating system."
 
   # validates on permit_steps#enter_repair
-  validates_inclusion_of :window, :in => [true, false], :if => :active_or_repair?, :message => "Please select whether you are changing windows in this project."
+  validates_inclusion_of :window, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are changing windows in this project."
   validates_numericality_of :window_count, greater_than: 0, :if => :only_if_window_true?, :message => "Please specify the number of windows you are changing/adding."
-  validates_inclusion_of :door, :in => [true, false], :if => :active_or_repair?, :message => "Please select whether you are changing doors in this project."
+  validates_inclusion_of :door, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are changing doors in this project."
   validates_numericality_of :door_count, greater_than: 0, :if=> :only_if_door_true?, :message => "Please specify the number of doors you are changing/adding."
-  validates_inclusion_of :wall, :in => [true, false], :if => :active_or_repair?, :message => "Please select whether you are changing walls in this project."
-  validates_inclusion_of :siding, :in => [true, false], :if => :active_or_repair?, :message => "Please select whether you are changing sidings in this project."
-  validates_inclusion_of :floor, :in => [true, false], :if => :active_or_repair?, :message => "Please select whether you are changing floors in this project."
+  validates_inclusion_of :wall, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are changing walls in this project."
+  validates_inclusion_of :siding, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are changing sidings in this project."
+  validates_inclusion_of :floor, :in => [true, false], :if => :active_or_details?, :message => "Please select whether you are changing floors in this project."
 
   def active?
     status == 'active'
@@ -48,30 +49,34 @@ class Permit < ActiveRecord::Base
   end
 
   def only_if_addition_presence?
-    active_or_addition? && ! addition_area.blank?
+    active_or_details? && ! addition_area.blank?
   end
 
   def only_if_house_presence?
-    active_or_addition? && ! house_area.blank?
+    active_or_details? && ! house_area.blank?
   end
 
   def only_if_job_cost_presence?
     active_or_details? && ! job_cost.blank?
   end
 
-  def active_or_addition?
-    status.to_s.include?('enter_addition') || active?
-  end
-
-  def active_or_repair?
-    status.to_s.include?('enter_repair') || active?
-  end
-
   def only_if_window_true?
-    active_or_repair? && window
+    active_or_details? && window
   end
 
   def only_if_door_true?
-    active_or_repair? && door
+    active_or_details? && door
+  end
+
+# def at_least_one_name
+#   if [self.zh_name, self.en_name, self.fr_name].reject(&:blank?).size == 0
+#     errors[:base] << ("Please choose at least one name - any language will do.")
+#   end
+# end 
+
+  def at_least_one_chosen
+    if !(addition || window || door || wall || siding || floor || cover || pool || deck || acs_struct)
+      errors[:base] << ("Plase choose an improvement.")
+    end
   end
 end
