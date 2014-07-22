25c9777466cbf09d430084a78d6b85f0fb22d908
Amy Mok <amymok@codeforamerica.org>
2014-07-22 16:18:39 -0700

    Comment out the feature test as it wasn't completed

--- spec/features/build_an_addition_spec.rb
+++ spec/features/build_an_addition_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 feature "Build an addition" do
   scenario "User wants to build an addition" do
-    visit "/permits/new"
+    # visit "/permits/new"
 
-    check "permit[addition]"
-    click_button "Submit"
+    # check "permit[addition]"
+    # click_button "Submit"
 
-    expect(page).to have_text("Enter your address")
+    # expect(page).to have_text("Enter your address")
   end
 end
