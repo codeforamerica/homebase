8056a85d20ad5b492a92b888b183db4bd5543c85
davidrleonard <dleonard@codeforamerica.org>
2014-07-23 16:33:23 -0700

    Kill old migrate

--- db/migrate/20140723232313_add_confirm_terms_fields.rb
+++
@@ -1,6 +0,0 @@
-class AddConfirmTermsFieldsToPermits < ActiveRecord::Migration
-  def change
-    add_column :permits, :confirmed_name, :string
-    add_column :permits, :accepted_terms, :boolean
-  end
-end
