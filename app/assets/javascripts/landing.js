3532c1041d951c5ff5a7727f38065207bf763761
davidrleonard <dleonard@codeforamerica.org>
2014-07-29 19:10:40 -0500

    Make .pick-[projectname] class toggle correctly when the user clicks the button

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       clickedButton = $(this).attr("id");
       buttonId = ("#").concat(clickedButton)
       chosenProject = (".").concat(clickedButton)
-      clickedButtonClass = ("pick-").concat(clickedButton);
+      clickedButtonClass = (".pick-").concat(clickedButton);
 
       $(chosenProject).toggleClass("displayed");
       $(clickedButtonClass).toggleClass("btn-success");
