179f85b97f0d97720b41f2d2d8533b1c1f5522ca
Amy Mok <amymok@codeforamerica.org>
2014-07-30 22:35:21 -0700

    Add functionality to only timeout when idle for 7 minutes

--- app/assets/javascripts/timeout_modal.js
+++ app/assets/javascripts/timeout_modal.js
@@ -1,20 +1,42 @@
 // timeout_modal
 
+var INTERVAL_TIME_IN_MS = 60000; // 1 minute
+var DISPLAY_MODAL_TIME_IN_S = 7;
+var RESET_TIME_IN_S = DISPLAY_MODAL_TIME_IN_S + 3;
+
+var idleTime = 0;
 $(document).ready(function($) 
   {
 
-    // call timeout modal after 7 minutes
-    function show_modal() 
+    // Only if not at the root
+    if (document.location.pathname != "/")
     {
-      $('#myModal').modal();
+      //Increment the idle time counter every minute.
+      var idleInterval = setInterval(timerIncrement, INTERVAL_TIME_IN_MS);
+
+      //Zero the idle timer on mouse movement.
+      $(this).mousemove(function (e) {
+          idleTime = 0;
+      });
+      $(this).keypress(function (e) {
+          idleTime = 0;
+      });
     }
-    window.setTimeout(show_modal, 420000); // 7 minute delay before it calls the modal function
-
-    // redirect to 1st step after 10 minutes
-    window.setTimeout(function() 
-      {
-        location.href = "http://app.homebasefix.com/";
-      },
-      600000)
-
 });
+
+// call timeout modal after 7 minutes
+function show_modal() 
+{
+  $('#myModal').modal();
+}
+
+function timerIncrement() {
+  idleTime++;
+  if (idleTime == DISPLAY_MODAL_TIME_IN_S)
+  {
+    show_modal();
+  } else if (idleTime == RESET_TIME_IN_S)
+  {
+    location.href = "http://" + document.location.host;
+  }
+}
