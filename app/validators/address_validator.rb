92792fabb544131d3049c740dfaa3bea7c50863f
Amy Mok <amymok@codeforamerica.org>
2014-09-08 16:35:28 -0700

    Change some error languages

--- app/validators/address_validator.rb
+++ app/validators/address_validator.rb
@@ -3,11 +3,13 @@ class AddressValidator < ActiveModel::Validator
 
     # @TODO: Check if this is valid
   	if record.lat != nil && record.lng != nil
-	    unless CosaBoundary.inCosa?(record.lat, record.lng)
-	      record.errors[:owner_address] << ("Sorry, the address you entered is not in San Antonio.  Please enter a San Antonio address.")
+      inCosa = CosaBoundary.inCosa?(record.lat, record.lng)
+      
+	    unless inCosa
+	      record.errors[:owner_address] << ("Sorry, we think we got your address wrong here. Would you mind double checking it's in San Antonio?")
 	    end
     else
-      record.errors[:owner_address] << ("Sorry, we can't validate your address.  Please try again later.")
+      record.errors[:owner_address] << ("Sorry, we can't validate your address right now.  Would you mind trying again later?")
 	  end
   end
 end
