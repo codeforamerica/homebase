6e8c901a10cdc8c8a1cebd2b89b1f47a81bea3be
Amy Mok <amymok@codeforamerica.org>
2014-09-17 16:47:11 -0700

    Fix siding spec

--- spec/features/replace_siding_spec.rb
+++ spec/features/replace_siding_spec.rb
@@ -11,34 +11,34 @@ feature "Replace sidings" do
     visit '/permits'
 
     # permit#new
-    check "Replace siding"
-    click_on "Next step"
+    check I18n.t('views.permits.new.project.siding')
+    click_on I18n.t('views.permits.new.submit')
 
     expect(current_path).to eq('/en/permit_steps/answer_screener')
-    expect(page).to have_content("Enter your project details")
+    expect(page).to have_content(I18n.t('views.permit_steps.answer_screener.header'))
 
     #permit_steps#answer_screener
     within "div.siding_over_existing" do
-      choose "Yes"
+      choose I18n.t('views.permit_steps.answer_screener.siding.options.yes_statement')
     end
 
     within "div.contractor" do
-      choose "I'm doing the work myself (with help my friends or family)"
+      choose I18n.t('views.permit_steps.answer_screener.contractor.options.no_statement')
     end
 
     within "div.owner_address" do
-      fill_in "Enter the address of your home you'll do work on.", with: "302 Madison St, San Antonio"
+      fill_in I18n.t('views.permit_steps.answer_screener.owner_address.question'), with: "302 Madison St, San Antonio"
     end
 
-    click_on "Submit"
+    click_on I18n.t('views.permit_steps.answer_screener.submit')
 
     expect(current_path).to eq('/en/permit_steps/display_permits')
-    expect(page).to have_content("This is how to start your project(s)")
+    expect(page).to have_content(I18n.t('views.permit_steps.display_permits.intro_text'))
 
     #permit_steps#display_permits
-    page.find('div.permit_not_needed').should have_content('Replace Siding')
+    page.find('div.permit_not_needed').should have_content(I18n.t('models.permit.siding.name'))
 
-    page.has_no_button? "Apply for this permit"
+    page.has_no_button? I18n.t('views.permit_steps.display_permits.submit')
 
   end
   
@@ -47,62 +47,62 @@ feature "Replace sidings" do
     visit '/permits'
 
     # permit#new
-    check "Replace siding"
-    click_on "Next step"
+    check I18n.t('views.permits.new.project.siding')
+    click_on I18n.t('views.permits.new.submit')
 
     expect(current_path).to eq('/en/permit_steps/answer_screener')
-    expect(page).to have_content("Enter your project details")
+    expect(page).to have_content(I18n.t('views.permit_steps.answer_screener.header'))
 
     #permit_steps#answer_screener
     within "div.siding_over_existing" do
-      choose "No"
+      choose I18n.t('views.permit_steps.answer_screener.siding.options.no_statement')
     end
 
     within "div.contractor" do
-      choose "I'm doing the work myself (with help my friends or family)"
+      choose I18n.t('views.permit_steps.answer_screener.contractor.options.no_statement')
     end
 
     within "div.owner_address" do
-      fill_in "Enter the address of your home you'll do work on.", with: "302 Madison St, San Antonio"
+      fill_in I18n.t('views.permit_steps.answer_screener.owner_address.question'), with: "302 Madison St, San Antonio"
     end
 
-    click_on "Submit"
+    click_on I18n.t('views.permit_steps.answer_screener.submit')
 
     expect(current_path).to eq('/en/permit_steps/display_permits')
-    expect(page).to have_content("This is how to start your project(s)")
+    expect(page).to have_content(I18n.t('views.permit_steps.display_permits.intro_text'))
 
     #permit_steps#display_permits
-    page.find('div.permit_needed').should have_content('Replace Siding')
+    page.find('div.permit_needed').should have_content(I18n.t('models.permit.siding.name'))
 
-    click_on "Apply for this permit"
+    click_on I18n.t('views.permit_steps.display_permits.submit')
 
     expect(current_path).to eq('/en/permit_steps/enter_details')
-    expect(page).to have_content("General Repair/Residential Permit Application")
+    expect(page).to have_content(I18n.t('views.permit_steps.enter_details.intro.heading'))
 
     #permit_steps#enter_details
 
-    fill_in "Name", with: "John Doe"
-    page.has_field?('Street address', with: "302 Madison St, San Antonio, TX 78204")
-    fill_in "Email address", with: "john@johndoe.com"
-    fill_in "Phone number", with: "413-456-3456"
+    fill_in I18n.t('views.permit_steps.enter_details.homeowner_info.name.question'), with: "John Doe"
+    page.has_field?(I18n.t('views.permit_steps.enter_details.homeowner_info.address.question'), with: "302 Madison St, San Antonio, TX 78204")
+    fill_in I18n.t('views.permit_steps.enter_details.homeowner_info.email.question'), with: "john@johndoe.com"
+    fill_in I18n.t('views.permit_steps.enter_details.homeowner_info.phone.question'), with: "413-456-3456"
 
-    fill_in "Work Summary", with: "Replace siding in my backyard"
-    fill_in "Job Cost", with: "10000"
+    fill_in I18n.t('views.permit_steps.enter_details.final_info.work_summary.question'), with: "Replace siding in my backyard"
+    fill_in I18n.t('views.permit_steps.enter_details.final_info.job_cost.question'), with: "10000"
 
-    click_on "Next step"
+    click_on I18n.t('views.permit_steps.enter_details.submit')
 
     expect(current_path).to eq('/en/permit_steps/confirm_terms')
-    expect(page).to have_content("Read these terms and sign your permit online")
+    expect(page).to have_content(I18n.t('views.permit_steps.confirm_terms.intro_text'))
 
     #permit_steps#confirm_terms
-    check "permit_accepted_terms"
-    fill_in "Enter your name", with: "John Doe"
+    check I18n.t('views.permit_steps.confirm_terms.requirement.accept_text')
+    fill_in I18n.t('views.permit_steps.confirm_terms.signature.confirmed_name.placeholder'), with: "John Doe"
 
-    click_on "I agree"
+    click_on I18n.t('views.permit_steps.confirm_terms.submit')
 
     expect(current_path).to eq('/en/permit_steps/display_summary')
-    expect(page).to have_content("Congrats! You filled out your permit application")
-    expect(page).not_to have_content("Make a detailed site plan")
+    expect(page).to have_content(I18n.t('views.permit_steps.display_summary.intro.heading'))
+    expect(page).not_to have_content(I18n.t('views.permit_steps.display_summary.site_plan.heading'))
 
   end
 
