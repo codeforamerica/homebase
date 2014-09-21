568469581930b80122860362a0ccfc9745b80f2e
Amy Mok <amymok@codeforamerica.org>
2014-09-20 21:24:22 -0700

    Add button feature and fixed image link in display_summary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
+  before_action :set_locale, :set_translation_path
  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
+
+  def set_translation_path
+    puts "before path: #{@path}"
+    @path = Rails.application.routes.recognize_path(request.path)
+    if @path[:locale] == 'es'
+      @path[:locale] = 'en'
+    elsif # path[:locale] == 'en'
+      @path[:locale] = 'es'
+    end
+
+    puts "this is path **********: #{@path}"
+
+  end
+
+
   def reset
     session.delete :selected_addition
     session.delete :selected_acs_struct
@@ -20,6 +34,7 @@ class ApplicationController < ActionController::Base
     session.delete :selected_siding
     session.delete :selected_floor
 
+    puts root_url
     redirect_to root_url
   end
 
