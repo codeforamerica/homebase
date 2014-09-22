65be142d64db4f7a9ff76fe52950628c2b3611d3
davidrleonard <dleonard@codeforamerica.org>
2014-09-22 16:14:20 -0700

    Define new layout and template for PermitSender mailer

--- app/mailers/permit_sender.rb
+++ app/mailers/permit_sender.rb
@@ -1,10 +1,13 @@
 class PermitSender < ActionMailer::Base
   default from: "donotreply@homebasefix.com"
+  layout 'email' # use views/layouts/email.html.erb as the base layout
 
   # send the permit email to San Antonio Gov, pass in the user object that contains the user's email address
   def send_permit_application(permit)
     @permit = permit
     mail( :to => @permit.email,
-    :subject => 'This is a test email, this is a test' )
+    :subject => 'This is a test email, this is a test',
+    :template_path => 'permit_sender',
+    :template_name => 'send_permit_application' )
   end
 end
