7ba75b88985f2a11baee2bdae39860243b6c165b
davidrleonard <dleonard@codeforamerica.org>
2014-09-22 13:42:04 -0700

    Create mailer files

---
+++ app/mailers/permit_sender.rb
@@ -0,0 +1,10 @@
+class PermitSender < ActionMailer::Base
+  default from: "from@example.com"
+
+  # send the permit email to San Antonio Gov, pass in the user object that contains the user's email address
+  def send_permit_application(user)
+    @permit = permit
+    mail( :to => @permit.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+end
